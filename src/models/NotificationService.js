6a6005c0b5aadc01b78776af7ec4d0b0a8bae1e3
Manuel Felipe Barrera Barrera <felipebarrera104@gmail.com>
2025-10-29 16:14:35 -0500

    Implement NotificationService class
    Added a NotificationService class to manage notifications.

--- src/models/NotificationService.js
+++ src/models/NotificationService.js
@@ -1,1 +1,11 @@
-
+/**
+ * NotificationService class to manage notifications and scheduled notifications.
+ * @param {String []} notifications - List of notifications.
+ * @param {object []} scheduledNotifications - List of scheduled notifications.
+ */
+class NotificationService {
+    constructor(notifications, scheduledNotifications) {
+        this.notifications = notifications;
+        this.scheduledNotifications = scheduledNotifications;
+    }
+}
