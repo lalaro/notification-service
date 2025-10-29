6e82551cadb243435bb218ec9b15a19f4f13a604
Manuel Felipe Barrera Barrera <felipebarrera104@gmail.com>
2025-10-29 16:15:06 -0500

    Implement NotificationRepository with CRUD methods

--- src/repositories/notificationRepository.js
+++ src/repositories/notificationRepository.js
@@ -1,1 +1,31 @@
 
+const Notification = require("../models/Notification");
+
+class NotificationRepository {
+  async listar() {
+    return Notification.find();
+  }
+
+  async listarPorUsuario(userId) {
+    return Notification.find({ userId });
+  }
+
+  async obtenerPorId(id) {
+    return Notification.findById(id);
+  }
+
+  async crear(data) {
+    const notification = new Notification(data);
+    return notification.save();
+  }
+
+  async actualizar(id, data) {
+    return Notification.findByIdAndUpdate(id, data, { new: true });
+  }
+
+  async eliminar(id) {
+    return Notification.findByIdAndDelete(id);
+  }
+}
+
+module.exports = new NotificationRepository();
